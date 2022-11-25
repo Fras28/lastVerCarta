7933ce589df98cdb397d44d9ad3044a1c35cdbad
Franco Selvarolo <fselvarolo28@gmail.com>
2022-11-25 09:15:40 -0300

    new model

--- src/Components/Cafeteria/Cafeteria.jsx
+++ src/Components/Cafeteria/Cafeteria.jsx
@@ -7,6 +7,8 @@ import { asyncallProducts } from "../redux/slice.jsx";
 
 
 export const Cafeteria = (id) => {
+
+
   let dispatch = useDispatch();
 
   let { allProduct } = useSelector((state) => state.alldata);
@@ -25,13 +27,31 @@ export const Cafeteria = (id) => {
   return (
     <div className="containerL">
       <Nav id={id.match.params.id} />
+      <div className="sectioner">
+        <a href="#1">Cafe</a>
+        <a href="#2">Cafe Especial</a>
+        <a href="#3">Infusiones</a>
+        <a href="#4">Cold Brew</a>
+        <a href="#5">Cafe Frio</a>
+        <a href="#6">Desayunos</a>
+      </div>
       <div className="conteinerLC">
         <div className="conteinerLB2">
+          <img
+            src="https://cdn.shopify.com/s/files/1/0613/6342/1411/files/a_900x900.png?v=1669254768"
+            alt="promo"
+          />
+          <div id="1"></div>
           <Cards products={Cafex} />
+          <div id="2"></div>
           <Cards products={CafeEspecial} />
+          <div id="3"></div>
           <Cards products={Infusionesx} />
+          <div id="4"></div>
           <Cards products={ColdBrew} />
+          <div id="5"></div>
           <Cards products={CafeFrio} />
+          <div id="6"></div>
           <Cards products={Desayunox} />
         </div>
       </div>
